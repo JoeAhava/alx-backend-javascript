eeaf159d1aca839aecd1e25605d3bf8587d733b2
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-09 19:50:20 +0300

    multi promise with reject

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,14 +3,23 @@ import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all(
-    [signUpUser(firstName, lastName),
-      uploadPhoto(fileName)],
-  ).then((v) => ([{
-    status: 200,
-    value: v[0].firstName,
-  },
-  {
-    status: 200,
-    value: v[0].lastName,
-  }])).catch((err) => ({ status: 400, value: err }));
+    [
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName).catch((err) => err),
+    ],
+  ).then((v) => {
+    const res = [];
+    for (const value of v) {
+      let status = 'sucess';
+      if (value instanceof Error) {
+        status = 'failed';
+      }
+
+      res.push({
+        status,
+        value,
+      });
+    }
+    return res;
+  });
 }
--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,3 +1,3 @@
 import handleProfileSignup from './6-final-user';
 
-console.log(handleProfileSignup('John', 'Doe', 'joe.jpg'));
+console.log(handleProfileSignup('John', 'Doe', 'joe.jpg').then((v) => console.log(v)));
