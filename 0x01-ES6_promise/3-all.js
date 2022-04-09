84a4eb870f0f42ec8aa8539632182b7c928f0151
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-09 19:00:25 +0300

    promise.all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  let result = '';
+  Promise.all([
+    uploadPhoto().then((v) => {
+      result += v.body;
+    }),
+    createUser().then((v) => {
+      result += ` ${v.firstName} ${v.lastName}`;
+    }),
+  ]).then(() => {
+    console.log(result);
+  });
+}
