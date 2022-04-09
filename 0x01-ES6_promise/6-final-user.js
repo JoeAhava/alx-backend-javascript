b7e4a7a2d297aa97417377fc86d5a3c187db29ea
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-09 19:55:15 +0300

    multi promise with reject

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,25 +1,49 @@
-import uploadPhoto from './5-photo-reject';
+// import uploadPhoto from './5-photo-reject';
+// import signUpUser from './4-user-promise';
+
+// export default function handleProfileSignup(firstName, lastName, fileName) {
+//   return Promise.all(
+//     [
+//       signUpUser(firstName, lastName),
+//       uploadPhoto(fileName).catch((err) => err),
+//     ],
+//   ).then((v) => {
+//     const res = [];
+//     for (const value of v) {
+//       let status = 'fulfilled';
+//       if (value instanceof Error) {
+//         status = 'failed';
+//       }
+
+//       res.push({
+//         status,
+//         value,
+//       });
+//     }
+//     return res;
+//   });
+// }
+
 import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all(
-    [
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName).catch((err) => err),
-    ],
-  ).then((v) => {
-    const res = [];
-    for (const value of v) {
-      let status = 'fulfilled';
-      if (value instanceof Error) {
-        status = 'failed';
-      }
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName).catch((error) => error),
+  ]).then((values) => {
+    let status;
 
-      res.push({
+    const responses = [];
+    values.forEach((value) => {
+      if (value instanceof Error) status = 'failed';
+      else status = 'fulfilled';
+      responses.push({
         status,
         value,
       });
-    }
-    return res;
+    });
+
+    return responses;
   });
 }
