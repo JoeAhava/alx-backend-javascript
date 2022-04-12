514f7c2619a606cb770ec51d966342eb69966244
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-12 21:02:40 +0300

    map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  return new Map([
+    { name: 'Apples', quantity: 10 },
+    { name: 'Tomatoes', quantity: 10 },
+    { name: 'Pasta', quantity: 1 },
+    { name: 'Rice', quantity: 1 },
+    { name: 'Banana', quantity: 5 },
+  ]);
+}
