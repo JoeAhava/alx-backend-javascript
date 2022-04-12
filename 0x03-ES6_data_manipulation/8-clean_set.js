c2992af20cad21ecf99bda284d1e3e7eacd4fd64
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-12 21:34:35 +0300

    cleanset fix 4

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,13 @@
 export default function cleanSet(set, startString) {
+  if (typeof set !== typeof new Set()) {
+    return '';
+  }
   try {
     const results = [];
     if (startString.length > 0) {
       set.forEach((value) => {
         if (value.startsWith(startString, 0)) {
+          console.log(value.slice(startString.length));
           results.push(value.slice(startString.length));
         }
       });
