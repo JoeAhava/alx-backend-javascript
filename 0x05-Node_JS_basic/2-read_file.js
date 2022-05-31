ca004f3c728c6eb93cdf2d4c334163b5c55222a7
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-31 23:59:24 +0300

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,11 +3,35 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const file = fs.readFileSync(path, { encoding: 'utf-8' });
-    const students = file.split('\n');
+    let students = file.split('\n');
     students.shift();
+    students = students.map((student) => student.split(','));
     // console.log(students);
-    return students.length;
+    const fields = students.map((std) => std[3]);
+    const fieldData = {};
+    // console.log(fields);
+    fields.forEach((value) => {
+      if (fieldData[value]) {
+        fieldData[value] += 1;
+      } else {
+        fieldData[value] = 1;
+      }
+    //   console.log(fieldData);
+    });
+    const fieldDataStd = Object(fieldData);
+    students.forEach((value) => {
+      if (fieldDataStd[value[3]].length > 0) {
+        fieldDataStd[value[3]].push(value[0]);
+      } else {
+        fieldDataStd[value[3]] = [value[0]];
+      }
+    //   console.log(fieldDataStd);
+    });
+    process.stdout.write(`Number of students: ${students.length}
+Number of students in CS: ${fieldData.CS.length}. List: ${fieldData.CS}
+Number of students in SWE: ${fieldData.SWE.length}. List: ${fieldData.SWE}\n`);
   } catch (err) {
+    console.log(err);
     throw new Error('Cannot load the database');
   }
 }
