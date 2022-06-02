fd19f71717b24bf60d3300944179cf330e23d3ff
Yohannes Terfa <lightshadow011@gmail.com>
2022-06-02 11:04:46 +0300

    http 5 fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,8 +5,6 @@ const app = http.createServer(async (req, res) => {
   switch (req.url) {
     case '/students': {
       res.setHeader('Content-Type', 'text/plain');
-      res.setHeader('Status', 200);
-      res.write('This is the list of our students\n');
       try {
         const data = await countStudents(process.argv[2]);
         let students = data.split('\n');
@@ -32,12 +30,15 @@ const app = http.createServer(async (req, res) => {
             default: break;
           }
         });
+        res.setHeader('Status', 200);
+        res.write('This is the list of our students\n');
         res.write(`Number of students: ${students.length}\n`);
         res.write(`Number of students in CS: ${cs}. List: ${csList.join(', ')}\n`);
         res.end(`Number of students in SWE: ${swe}. List: ${sweList.join(', ')}`);
         break;
       } catch (err) {
         res.setHeader('Status', 500);
+        res.write('This is the list of our students\n');
         res.end('Cannot load the database');
       }
 
