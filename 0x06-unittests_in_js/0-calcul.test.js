12ea3c576c844875f0fad78377bbf7e06bc6934c
Yohannes Terfa <lightshadow011@gmail.com>
2022-06-03 22:16:20 +0300

    test 1 fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,14 +1,23 @@
-const calculateNumber = require('./0-calcul')
-const assert = require('assert');
+const calculateNumber = require("./0-calcul");
+const assert = require("assert");
 
-describe('calculateNumber', () => {
-    it('calculate tests', () => {
+describe("calculateNumber", () => {
+    it("both int", () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1.2, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(4.5, 1.4), 6);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    })
-  
+    });
+    it("first float", () => {
+        assert.strictEqual(calculateNumber(1.2, 3), 4);
+    });
+    it("second float", () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+    it("both float", () => {
+        assert.strictEqual(calculateNumber(4.5, 1.4), 6);
+    });
+    it("both float", () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+    it("both float", () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
 });
