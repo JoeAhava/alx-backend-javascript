4df3e392f8273f3feb2630457a2bf3c86511afcf
Yohannes Terfa <lightshadow011@gmail.com>
2022-06-03 23:27:50 +0300

    test with stub

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,14 @@
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require("./utils");
+const sinon = require("sinon");
+const expect = require("chai").expect;
+
+describe("testing with sinon spy", () => {
+	it("lets see spy", () => {
+		const spy = sinon.stub(Utils, "calculateNumber");
+		const payment = sendPaymentRequestToApi(12, 3);
+		expect(spy.calledOnce).to.be.true;
+		expect(spy.calledWith("SUM", 12, 3)).to.be.true;
+		spy.restore();
+	});
+});
